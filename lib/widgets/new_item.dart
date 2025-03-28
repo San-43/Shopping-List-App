93924d5c9507835b5e7c0e1840bef43e45c00539
Sandro Martínez <virgosmp.mx@gmail.com>
2025-03-27 23:26:43 -0600

    updated new_item.dart

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -21,11 +21,11 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https('shopping-list-app-9ce0c-default-rtdb.firebaseio.com', 'shopping-list.json');
-      http.post(url, headers:  {
+      final response = await http.post(url, headers:  {
         'Content-Type': 'application/json',
       }, body: json.encode({
         'name': _enteredName,
